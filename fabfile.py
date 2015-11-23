1e4dcb8f8a2b00259dce60e3ee588422afe6525a
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2015-11-23 14:50:23 +0100

    [fabric] Add fabfile.py file to project

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""The sagemathcell stack fabric file"""
+
+from fabric.api import local
+
+def git():
+    """Setup Git"""
+
+    local("git remote rm origin")
+    local("git remote add origin https://korniichuk@github.com/korniichuk/sagemathcell.git")
+    local("git remote add bitbucket https://korniichuk@github.com/korniichuk/sagemathcell.git")
